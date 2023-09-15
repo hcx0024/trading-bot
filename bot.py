549a27bca1eb1de8725b64384ab81fe4c3dbb730
hcx0024 <amyhu424323@gmail.com>
2023-09-14 22:24:22 -0400

    Write scheme of bot

--- bot.py
+++ bot.py
@@ -1,1 +1,31 @@
-print("hello world")
+#define asset
+
+#INITIAL CHECK
+#check position
+#check if tradable
+
+#GENERAL TREND
+#load 30mins candle
+#perform general trend analysis
+
+#LOOP
+#load 5 mins data
+#perform instant trend analysis
+#perform RSI analysis
+#perform stochastic analysis
+
+#SUBMIT ORDER
+#submit order
+#check position
+
+#"ENTER POSITION" MODE (LOOP)
+#check take profit
+#check stop loss
+#check stock crossing
+
+#GET OUT
+#check order
+#check position
+
+#wait 15mins
+#back to beginning
